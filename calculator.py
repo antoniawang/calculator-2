c359ac7d39177f3e216309b0fa5e42f4beb93e55
Hackbright Student <info@hackbrightacademy.com>
2015-07-01 18:05:01 -0700

    create function for calculator

---
+++ calculator.py
@@ -0,0 +1,21 @@
+"""
+calculator.py
+
+Using our arithmetic.py file from Exercise02, create the
+calculator program yourself in this file.
+"""
+
+from arithmetic import *
+list_of_operators = ['+','-','*','/','square','cube','pow','mod','q']
+list_of_functions = [add, subtract, multiply, divide, square, cube, power, mod]
+
+while True:
+    user_input = raw_input()
+    tokens = user_input.split(" ")
+    print tokens
+
+    # for operator in list_of_operators:
+    for i in range(len(list_of_operators)):
+        if tokens[0] == list_of_operators[i]:
+        # if tokens[0] == operator :
+            print function[i]((float(tokens[1]),float(tokens[2]))
